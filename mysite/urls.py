3d7aadd03738c6771aee4ab4ee02565e6d0068df
GuillermoEstevez <guillermo.estevez@hotmail.com>
2016-04-05 17:58:27 -0300

    Cambie el HTML para la página.

--- mysite/urls.py
+++ mysite/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    
+    #Django ahora redirigirá todo lo que vaya hacia 'http://127.0.0.1:8000/' a blog.urls y buscará más instrucciones allí.
+    url(r'', include('blog.urls')),
 ]
